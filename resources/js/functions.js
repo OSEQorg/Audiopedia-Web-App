59c9c00523f630c97526cd6bf3c6f96ffb777310
Marcel Heyne <32398058+marcelheyne@users.noreply.github.com>
2021-09-08 10:48:02 +0200

    Add files via upload

---
+++ resources/js/functions.js
@@ -0,0 +1,26 @@
+Amplitude.init({
+    "songs": [
+      {
+        "name": "[1] COVID-19",
+        "url": "./resources/mp3/1.mp3",
+      },
+            {
+        "name": "[2] COVID Song",
+        "url": "./resources/mp3/2.mp3",
+      },
+      {
+        "name": "[3] Leprosy",
+        "url": "./resources/mp3/3.mp3",
+      },
+      {
+        "name": "[4] Leprosy Song",
+        "url": "./resources/mp3/4.mp3",
+      },
+      {
+        "name": "[5] WASH",
+        "url": "./resources/mp3/5.mp3",
+      }
+      
+    ]
+  });
+
